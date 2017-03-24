f77335d7a514e93a2c7f1729578bd8ea72c531b6
quannypete <younghampton@gmail.com>
2017-03-24 18:03:52 -0400

    Add files via upload

---
+++ Sec1-BasicsAndPrinting/LetterToYourself.java
@@ -0,0 +1,23 @@
+
+public class LetterToYourself {
+	public static void main( String [] args){
+	/*
+	 * A Letter to Yourself
+	 * http://programmingbydoing.com/a/letter-to-yourself.html
+	 * Write a program that displays your name and address on the screen as if it were a letter. Your output should look
+	 * something like that below.
+	 */
+		
+		System.out.println("+---------------------------------------------------------+");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                         |");
+		System.out.println("|                                                         |");
+		System.out.println("|                              Quanny Hampton             |");
+		System.out.println("|                              1738 My Way                |");
+		System.out.println("|                              Bronx, NY 10037            |");
+		System.out.println("|                                                         |");
+		System.out.println("+---------------------------------------------------------+");
+	}
+}
