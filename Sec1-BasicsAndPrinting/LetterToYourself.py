bf4939e8a48eb5c9a7a38b037cfce7d15c595fd0
quannypete <younghampton@gmail.com>
2017-03-24 17:48:42 -0400

    Create LetterToYourself.py

---
+++ Sec1-BasicsAndPrinting/LetterToYourself.py
@@ -0,0 +1,16 @@
+# A Letter to Yourself
+# http://programmingbydoing.com/a/letter-to-yourself.html
+# Write a program that displays your name and address on the screen as if it were a letter. Your output should look
+# something like that below.
+
+print"+---------------------------------------------------------+"
+print"|                                                    #### |"
+print"|                                                    #### |"
+print"|                                                    #### |"
+print"|                                                         |"
+print"|                                                         |"
+print"|                              Quanny Hampton             |"
+print"|                              1738 My Way                |"
+print"|                              Bronx, NY 10037            |"
+print"|                                                         |"
+print"+---------------------------------------------------------+"
