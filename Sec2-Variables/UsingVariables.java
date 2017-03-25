deb84a6a9283db6948387afb1cfb113300029005
Quanny Hampton <younghampton@gmail.com>
2017-03-25 18:23:24 -0400

    Add files via upload

---
+++ Sec2-Variables/UsingVariables.java
@@ -0,0 +1,22 @@
+
+public class UsingVariables {
+	public static void main( String[] args){
+		/**
+		 * Using Variables
+		 * http://programmingbydoing.com/a/using-variables.html
+		 * Write a program that creates three variables: an int, a double, and a String.
+		 * Put the value 113 into the first variable, the value 2.71828 into the second, and the value "Computer Science" into
+		 * the third. It does not matter what you call the variables... this time.
+		 * Then, display the values of these three variables on the screen, one per line.
+		 * You must use three variables. Your program will probably have nine lines of code inside the curly braces of main().
+		 */
+		
+		int a= 113;
+		double b=  2.71828;
+		String subject= "Computer Science";
+		
+		System.out.println("This is room number # " + a);
+		System.out.println("e is close to " + b);
+		System.out.println("I am learn a bit about "+ subject);
+	}
+}
