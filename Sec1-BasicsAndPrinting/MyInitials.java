8a422e7d06945fa1c943055a67fb96e00fe92415
Quanny Hampton <younghampton@gmail.com>
2017-03-24 22:59:10 -0400

    Add files via upload

---
+++ Sec1-BasicsAndPrinting/MyInitials.java
@@ -0,0 +1,18 @@
+
+public class MyInitials {
+	public static void main( String [] args){
+		/* 
+		 * Your Initials
+		 * http://programmingbydoing.com/a/your-initials.html
+		 * Display your initials on the screen in block letters as shown.
+		 */
+		
+		System.out.println(" QQQ      ZZZZZ    H   H");
+		System.out.println("Q   Q         Z    H   H");
+		System.out.println("Q   Q        Z     H   H");
+		System.out.println("Q   Q       Z      HHHHH");
+		System.out.println("Q   Q      Z       H   H");
+		System.out.println("Q  Q      Z        H   H");
+		System.out.println(" QQ  Q    ZZZZZ    H   H");
+	}
+}
