5332b5e041a658946fac5ab5f357a42ac87ae130
Quanny Hampton <younghampton@gmail.com>
2017-03-24 22:44:15 -0400

    Add files via upload

---
+++ Sec1-BasicsAndPrinting/MyInitials.py
@@ -0,0 +1,9 @@
+print "For the name Quanny Z Hampton"
+
+print " QQQ      ZZZZZ    H   H"
+print "Q   Q         Z    H   H"
+print "Q   Q        Z     H   H"
+print "Q   Q       Z      HHHHH"
+print "Q   Q      Z       H   H"
+print "Q  Q      Z        H   H"
+print " QQ  Q    ZZZZZ    H   H"
