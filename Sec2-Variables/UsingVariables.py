6d58930c892b8f3b08afd5a4b09c3990f9dcf0a4
Quanny Hampton <younghampton@gmail.com>
2017-03-25 18:03:37 -0400

    Create UsingVariables.py

---
+++ Sec2-Variables/UsingVariables.py
@@ -0,0 +1,14 @@
+# Using Variables
+# http://programmingbydoing.com/a/using-variables.html
+# Write a program that creates three variables: an int, a double, and a String.
+# Put the value 113 into the first variable, the value 2.71828 into the second, and the value "Computer Science" into
+# the third. It does not matter what you call the variables... this time.
+# Then, display the values of these three variables on the screen, one per line.
+# You must use three variables. Your program will probably have nine lines of code inside the curly braces of main().
+
+a= 113
+float= 2.71828
+subject= "Computer Science"
+print "This is room # %d" % a
+print "e is close to %f" % float
+print "I am learning a bit about %s" % subject
