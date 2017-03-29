54681baddc989baebd9f2b9c4e178eba149b52d2
Quanny Hampton <younghampton@gmail.com>
2017-03-29 00:05:30 -0400

    Add files via upload

---
+++ Sec2-Variables/StillUsingVariables.py
@@ -0,0 +1,11 @@
+# Still Using Variables
+ # http://programmingbydoing.com/a/still-using-variables.html
+# Write a program that stores your name and year of graduation into variables, and displays their values on the screen.
+ # Make sure that you use two variables, and that the variable that holds your name is the best type for such a
+ # variable, and that the variable that holds the year is the best type for that variable.
+# Also make sure that your variable names are good: the name of a variable should always relate to its contents.
+
+name = "Quanny Hampton"
+grad_year = 2020
+
+print "My name is %s and I'll graduate in %d." % (name, grad_year)
