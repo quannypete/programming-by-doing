03f70c4588d3047dd2e1a5015442bcd79bdbbd77
Quanny Hampton <younghampton@gmail.com>
2017-03-29 00:16:49 -0400

    Add files via upload

---
+++ Sec2-Variables/StillUsingVariables.java
@@ -0,0 +1,19 @@
+
+public class StillUsingVariables {
+	public static void main( String[] args) {
+		
+		/**
+		 * Still Using Variables
+		 * http://programmingbydoing.com/a/still-using-variables.html
+		 * Write a program that stores your name and year of graduation into variables, and displays their values on the screen.
+		 * Make sure that you use two variables, and that the variable that holds your name is the best type for such a
+		 * variable, and that the variable that holds the year is the best type for that variable.
+		 * Also make sure that your variable names are good: the name of a variable should always relate to its contents.
+		 */
+		
+		String name = "Quanny Hampton";
+		int gradYear = 2020;
+		System.out.println("My name is " + name + "and I'll graduate in " + gradYear);
+		
+	}
+}
