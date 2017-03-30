4c3357205def32549a08f81ebea635d157e59e8e
Quanny Hampton <younghampton@gmail.com>
2017-03-30 00:39:45 -0400

    Create ForgetfulMachine.py

---
+++ Sec3-KeyboardInput/ForgetfulMachine.py
@@ -0,0 +1,11 @@
+# The Forgetful Machine
+# http://programmingbydoing.com/a/the-forgetful-machine.html
+# Ask the user for two words and two numbers, and let the person at the keyboard type in some values, but don't bother
+# storing their responses into any variables.
+# Again, there is no need to create any variables, except for the Scanner variable typically named keyboard.
+
+raw_input("Give me a word\n")
+raw_input("Give me another word\n")
+raw_input("Great, now your favorite number?\n")
+raw_input("And your second-favorite number...\n")
+print "Whew! Wasn't that fun"
