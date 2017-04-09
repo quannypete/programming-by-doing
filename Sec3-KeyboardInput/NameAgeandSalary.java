5194d91c1b955b4d5608578ea08bed1394a6ceaf
Quanny Hampton <younghampton@gmail.com>
2017-04-09 13:31:21 -0400

    Add files via upload

---
+++ Sec3-KeyboardInput/NameAgeandSalary.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class NameAgeandSalary {
+	public static void main(String[] args){
+		/**
+		 * Name, Age, and Salary
+		 * http://programmingbydoing.com/a/name-age-and-salary.html
+		 * Ask the user for their name. Then display their name to prove that you can recall it. Ask them for their age. Then
+		 * display that. Finally, ask them for how much they make and display that. You should use the most appropriate data
+		 * type for each variable.
+		 */
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		String name;
+		int age;
+		double salary;
+		
+		System.out.println("Hello. What is your name? ");
+		name = keyboard.next();
+		
+		System.out.println("Hi, " + name + " how old are you? ");
+		age = keyboard.nextInt();
+		
+		System.out.println("So you're " + age + " , eh? That's not old at all!");
+		
+		System.out.println("How much do you make, " + name + "?");
+		salary = keyboard.nextDouble();
+		
+		System.out.println(salary + "! I hope that's per hour and not per year! LOL!");
+	}
+}
