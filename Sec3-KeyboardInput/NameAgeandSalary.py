a2b8446e728b592d78b668a0507ef0c773f412b1
Quanny Hampton <younghampton@gmail.com>
2017-04-09 13:39:01 -0400

    Update NameAgeandSalary.py

--- Sec3-KeyboardInput/NameAgeandSalary.py
+++ Sec3-KeyboardInput/NameAgeandSalary.py
@@ -1,3 +1,9 @@
+# Name, Age, and Salary
+# http://programmingbydoing.com/a/name-age-and-salary.html
+# Ask the user for their name. Then display their name to prove that you can recall it. Ask them for their age. Then
+# display that. Finally, ask them for how much they make and display that. You should use the most appropriate data
+# type for each variable.
+
 print "Hello. What is your name?"
 name = raw_input()
 
