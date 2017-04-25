8413f07cdb11054534d5a1b2017ade2d71111854
Quanny Hampton <younghampton@gmail.com>
2017-04-24 21:24:26 -0400

    Update HowOldAreYouSpecifically.py

--- IfStatements/HowOldAreYouSpecifically.py
+++ IfStatements/HowOldAreYouSpecifically.py
@@ -18,6 +18,8 @@ age = raw_input()
 print ""
 if age < "16":
     print "You can't drive, %s." % name
+elif age "16" or age == "17":
+    print "You can drive but not vote, %s" % name
 elif age < "18":
     print "You can't vote, %s." % name
 elif age < "25":
