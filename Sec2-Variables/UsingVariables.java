eb52ca72e856c93abb5635d3d97dd133904ab50f
Quanny Hampton <younghampton@gmail.com>
2017-10-08 02:57:49 -0400

    Update UsingVariables.java

--- Sec2-Variables/UsingVariables.java
+++ Sec2-Variables/UsingVariables.java
@@ -1,12 +1,12 @@
 /**
-		 * Using Variables
-		 * http://programmingbydoing.com/a/using-variables.html
-		 * Write a program that creates three variables: an int, a double, and a String.
-		 * Put the value 113 into the first variable, the value 2.71828 into the second, and the value "Computer Science" into
-		 * the third. It does not matter what you call the variables... this time.
-		 * Then, display the values of these three variables on the screen, one per line.
-		 * You must use three variables. Your program will probably have nine lines of code inside the curly braces of main().
-		 */
+* Using Variables
+* http://programmingbydoing.com/a/using-variables.html
+* Write a program that creates three variables: an int, a double, and a String.
+* Put the value 113 into the first variable, the value 2.71828 into the second, and the value "Computer Science" into
+* the third. It does not matter what you call the variables... this time.
+* Then, display the values of these three variables on the screen, one per line.
+* You must use three variables. Your program will probably have nine lines of code inside the curly braces of main().
+*/
 
 public class UsingVariables {
 	public static void main( String[] args){
