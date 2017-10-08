4ba6b28aaef9ba680edb58a1903834ed17fe797b
Quanny Hampton <younghampton@gmail.com>
2017-10-08 03:00:36 -0400

    Update LetterToYourself.java

--- Sec1-BasicsAndPrinting/LetterToYourself.java
+++ Sec1-BasicsAndPrinting/LetterToYourself.java
@@ -7,7 +7,6 @@
 
 public class LetterToYourself {
 	public static void main( String [] args){
-
 		
 		System.out.println("+---------------------------------------------------------+");
 		System.out.println("|                                                    #### |");
