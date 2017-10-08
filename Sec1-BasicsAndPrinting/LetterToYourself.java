10a391cdbbdb3a504c3dfb0a8bd97e3d45d5a5d3
Quanny Hampton <younghampton@gmail.com>
2017-10-08 03:00:05 -0400

    Update LetterToYourself.java

--- Sec1-BasicsAndPrinting/LetterToYourself.java
+++ Sec1-BasicsAndPrinting/LetterToYourself.java
@@ -1,9 +1,10 @@
 /*
-	 * A Letter to Yourself
-	 * http://programmingbydoing.com/a/letter-to-yourself.html
-	 * Write a program that displays your name and address on the screen as if it were a letter. Your output should look
-	 * something like that below.
-	 */
+* A Letter to Yourself
+* http://programmingbydoing.com/a/letter-to-yourself.html
+* Write a program that displays your name and address on the screen as if it were a letter. Your output should look
+* something like that below.
+*/
+
 public class LetterToYourself {
 	public static void main( String [] args){
 
