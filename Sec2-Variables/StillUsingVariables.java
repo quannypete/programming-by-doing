56611d835788ae70b5062d71bedc99e5ebb4b58e
Quanny Hampton <younghampton@gmail.com>
2017-10-08 02:58:48 -0400

    Update StillUsingVariables.java

--- Sec2-Variables/StillUsingVariables.java
+++ Sec2-Variables/StillUsingVariables.java
@@ -1,11 +1,11 @@
 /**
-		 * Still Using Variables
-		 * http://programmingbydoing.com/a/still-using-variables.html
-		 * Write a program that stores your name and year of graduation into variables, and displays their values on the screen.
-		 * Make sure that you use two variables, and that the variable that holds your name is the best type for such a
-		 * variable, and that the variable that holds the year is the best type for that variable.
-		 * Also make sure that your variable names are good: the name of a variable should always relate to its contents.
-		 */
+* Still Using Variables
+* http://programmingbydoing.com/a/still-using-variables.html
+* Write a program that stores your name and year of graduation into variables, and displays their values on the screen.
+* Make sure that you use two variables, and that the variable that holds your name is the best type for such a
+* variable, and that the variable that holds the year is the best type for that variable.
+* Also make sure that your variable names are good: the name of a variable should always relate to its contents.
+*/
 
 public class StillUsingVariables {
 	public static void main( String[] args) {
