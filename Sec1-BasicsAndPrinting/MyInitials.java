f12857fc7b07b69bcad074488419503c7dc29f71
Quanny Hampton <younghampton@gmail.com>
2017-10-08 03:01:28 -0400

    Update MyInitials.java

--- Sec1-BasicsAndPrinting/MyInitials.java
+++ Sec1-BasicsAndPrinting/MyInitials.java
@@ -1,12 +1,11 @@
 /* 
-		 * Your Initials
-		 * http://programmingbydoing.com/a/your-initials.html
-		 * Display your initials on the screen in block letters as shown.
-		 */
+* Your Initials
+* http://programmingbydoing.com/a/your-initials.html
+* Display your initials on the screen in block letters as shown.
+*/
 
 public class MyInitials {
 	public static void main( String [] args){
-		
 		
 		System.out.println(" QQQ      ZZZZZ    H   H");
 		System.out.println("Q   Q         Z    H   H");
