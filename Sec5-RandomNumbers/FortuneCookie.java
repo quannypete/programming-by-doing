86cdc893426ec99c35df7e0e8a10b444770e9d66
Quanny Hampton <younghampton@gmail.com>
2017-10-14 16:04:16 -0400

    Update FortuneCookie.java

--- Sec5-RandomNumbers/FortuneCookie.java
+++ Sec5-RandomNumbers/FortuneCookie.java
@@ -1,3 +1,7 @@
+/*
+* Write a program that simulates a random fortune from a fortune cookie. You must have at least six fortunes.
+* For bonus points, also add randomly-chosen lotto numbers to the fortune. In Texas, the lotto chooses 6 numbers, each from 1-54.
+*/
 import java.util.Random;
 
 public class FortuneCookie
