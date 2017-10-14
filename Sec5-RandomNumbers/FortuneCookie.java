5c1d773751aa21eaf6bcfc9a17d071a9d0b695d1
Quanny Hampton <younghampton@gmail.com>
2017-10-14 15:59:45 -0400

    Add files via upload

---
+++ Sec5-RandomNumbers/FortuneCookie.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class FortuneCookie
+{
+	public static void main( String[] args )
+	{
+
+		Random rand = new Random();
+
+		int fortune = 1 + rand.nextInt(6);
+		String response = " ";
+
+		int num = 1 + rand.nextInt(54);
+
+		if (fortune == 1 )
+			response = "Good Things comes to those who wait.";
+		else if (fortune == 2 )
+				response = "You will run into an old friend.";
+		else if (fortune == 3 )
+				response = "True love is on its way. Make room!";
+		else if ( fortune == 4 )
+					response = "Focus on your health and well-being now. It won't wait.";
+		else if (fortune == 5 )
+					response = "Respect your elders. You could inherit a large sum of money.";
+		else
+			response = "Work with what you have.";
+
+		System.out.println("Fortune cookie says: " + response );
+
+		System.out.print("   ");
+        System.out.print(1 + rand.nextInt(54));
+        System.out.print(" - ");
+        System.out.print(1 + rand.nextInt(54));
+        System.out.print(" - ");
+        System.out.print(1 + rand.nextInt(54));
+        System.out.print(" - ");
+        System.out.print(1 + rand.nextInt(54));
+        System.out.print(" - ");
+        System.out.print(1 + rand.nextInt(54));
+        System.out.print(" - ");
+        System.out.print(1 + rand.nextInt(54));
+
+	}
+}
