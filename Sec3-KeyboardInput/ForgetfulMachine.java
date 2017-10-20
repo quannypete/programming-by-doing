e60a1b510dab8468ccebc79c492bd153980050ba
Quanny Hampton <younghampton@gmail.com>
2017-10-19 23:15:53 -0400

    Update ForgetfulMachine.java

--- Sec3-KeyboardInput/ForgetfulMachine.java
+++ Sec3-KeyboardInput/ForgetfulMachine.java
@@ -1,13 +1,13 @@
+/**
+* The Forgetful Machine
+* http://programmingbydoing.com/a/the-forgetful-machine.html
+* Ask the user for two words and two numbers, and let the person at the keyboard type in some values, but don't bother
+* storing their responses into any variables.
+* Again, there is no need to create any variables, except for the Scanner variable typically named keyboard.
+*/
 import java.util.Scanner;
 public class ForgetfulMachine {
 	public static void main( String [] args){
-		/**
-		 * The Forgetful Machine
-		 * http://programmingbydoing.com/a/the-forgetful-machine.html
-		 * Ask the user for two words and two numbers, and let the person at the keyboard type in some values, but don't bother
-		 * storing their responses into any variables.
-		 * Again, there is no need to create any variables, except for the Scanner variable typically named keyboard.
-		 */
 		
 		Scanner keyboard = new Scanner(System.in);
 		
