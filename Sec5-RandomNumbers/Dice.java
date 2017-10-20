7c6d219af4a16e340337c66c0967f19c9c6abc6f
Quanny Hampton <younghampton@gmail.com>
2017-10-19 22:53:50 -0400

    Update Dice.java

--- Sec5-RandomNumbers/Dice.java
+++ Sec5-RandomNumbers/Dice.java
@@ -1,3 +1,8 @@
+/*
+* Write a program that simulates a dice roll by picking a random number from 1-6 
+* and then picking a second random number from 1-6. Add the two values together, 
+* and display the total.
+*/
 import java.util.Random;
 public class Dice {
 	public static void main(String [] args){
