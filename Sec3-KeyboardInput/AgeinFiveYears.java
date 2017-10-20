520bae80e1b2f1baa254a80baf34954a521aafde
Quanny Hampton <younghampton@gmail.com>
2017-10-19 23:12:52 -0400

    Update AgeinFiveYears.java

--- Sec3-KeyboardInput/AgeinFiveYears.java
+++ Sec3-KeyboardInput/AgeinFiveYears.java
@@ -1,12 +1,12 @@
+/**
+* Age in Five Years
+* http://programmingbydoing.com/a/age-in-five-years.html
+* Ask the user for their name. Then display their name to prove that you can recall it. Ask them for their age. Then
+* display what their age would be five years from now. Then display what their age would be five years ago.
+*/
 import java.util.Scanner;
 public class AgeinFiveYears {
 	public static void main(String [] args){
-		/**
-		 * Age in Five Years
-		 * http://programmingbydoing.com/a/age-in-five-years.html
-		 * Ask the user for their name. Then display their name to prove that you can recall it. Ask them for their age. Then
-		 * display what their age would be five years from now. Then display what their age would be five years ago.
-		 */
 		
 		Scanner keyboard = new Scanner(System.in);
 		
