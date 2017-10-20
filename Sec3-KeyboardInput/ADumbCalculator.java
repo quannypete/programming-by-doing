51d7b09ddabf83e1c9f6b1b931ec0a048325fbde
Quanny Hampton <younghampton@gmail.com>
2017-10-19 23:11:43 -0400

    Update ADumbCalculator.java

--- Sec3-KeyboardInput/ADumbCalculator.java
+++ Sec3-KeyboardInput/ADumbCalculator.java
@@ -1,13 +1,12 @@
+/**
+* Dumb Calculator
+* http://programmingbydoing.com/a/a-dumb-calculator.html
+* Make a simple numeric calculator. It should prompt the user for three numbers. Then add the numbers together and
+* divide by 2. Display the result. Your program must support numbers with decimals and not just integers.
+*/
 import java.util.Scanner;
 public class ADumbCalculator {
 	public static void main(String [] args){
-	
-		/**
-		 * Dumb Calculator
-		 * http://programmingbydoing.com/a/a-dumb-calculator.html
-		 * Make a simple numeric calculator. It should prompt the user for three numbers. Then add the numbers together and
-		 * divide by 2. Display the result. Your program must support numbers with decimals and not just integers.
-		 */
 		
 		Scanner keyboard = new Scanner(System.in);
 		double first_number;
