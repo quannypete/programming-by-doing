904cbe18d8daf1825e42fcf0be7e2b2c6368d501
Quanny Hampton <younghampton@gmail.com>
2017-11-01 02:33:10 -0400

    Update DoubleDice.java

--- Sec6-WhileLoops/DoubleDice.java
+++ Sec6-WhileLoops/DoubleDice.java
@@ -1,3 +1,10 @@
+/*
+* Dice Double
+* https://programmingbydoing.com/a/dice-doubles.html
+* Modify your dice game from last time so that it keeps rolling until they get doubles (the same number on both dice). 
+* Notice that since there's no user input, this will happen very quickly (all the rolls will happen one right after the other).
+*/
+
 import java.util.Random;
 public class DoubleDice {
 	public static void main(String [] args){
