e75c4e105c2e1bb22f289366619996800741e8b1
Quanny Hampton <younghampton@gmail.com>
2017-11-01 22:11:12 -0400

    Update HiLo.java

--- Sec6-WhileLoops/HiLo.java
+++ Sec6-WhileLoops/HiLo.java
@@ -1,3 +1,14 @@
+/*
+* Hi-Lo with Limited Tries
+* https://programmingbydoing.com/a/hi-lo-with-limited-tries.html
+* Write a program that picks a random number from 1-100.
+* The user keeps guessing as long as their guess is wrong, and they've guessed less than 7 times. 
+* If their guess is higher than the number, say "Too high." If their guess is lower than the number, say "Too low."
+* If their guess is lower than the number, say "Too low."
+* When they get it right, the game stops. Or, if they hit seven guesses, the game stops even if they never got it right.
+* This means your while loop will have a compound condition using &&.
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 
