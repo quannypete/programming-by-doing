abab3e100730bf70399a8fceec60c8fc6e805829
Quanny Hampton <younghampton@gmail.com>
2017-11-01 23:04:20 -0400

    Create BasicArrays0.java

---
+++ Sec13-Arrays/BasicArrays0.java
@@ -0,0 +1,27 @@
+/*
+* Basic Arrays 0
+* https://programmingbydoing.com/a/basic-arrays-0.html
+* Create an array that can hold ten integers.
+* Fill up each slot of the array with the number -113. 
+* Then display the contents of the array on the screen.
+* Do not use a loop. 
+* Also, do not use any variable for the index; you must use literal numbers to refer to each slot.
+*/
+
+public class BasicArrays0{
+  public static void main(String[] args) {
+
+  int [] slot={-113,-113,-113,-113,-113,-113,-113,-113,-113,-113,-113};
+
+System.out.println("Slot 0 contains a " +slot[0]);
+System.out.println("Slot 1 contains a " +slot[1]);
+System.out.println("Slot 2 contains a " +slot[2]);
+System.out.println("Slot 3 contains a " +slot[3]);
+System.out.println("Slot 4 contains a " +slot[4]);
+System.out.println("Slot 5 contains a " +slot[5]);
+System.out.println("Slot 6 contains a " +slot[6]);
+System.out.println("Slot 7 contains a " +slot[7]);
+System.out.println("Slot 8 contains a " +slot[8]);
+System.out.println("Slot 9 contains a " +slot[9]);
+  }
+}
