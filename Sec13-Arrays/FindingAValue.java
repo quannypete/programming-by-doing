79bd456c250876c9a4df50e758f6d6dfbf6e485b
Quanny Hampton <younghampton@gmail.com>
2017-11-03 07:46:04 -0400

    Update FindingAValue.java

--- Sec13-Arrays/FindingAValue.java
+++ Sec13-Arrays/FindingAValue.java
@@ -1,3 +1,13 @@
+/*
+* Finding a Value in an Array
+* https://programmingbydoing.com/a/finding-a-value-in-an-array.html
+* Create an array that can hold ten integers, and fill each slot with a different random value from 1-50.
+* Display those values on the screen, and then prompt the user for an integer. 
+* Search through the array, and if the item is present, say so.
+* It is not necessary to display anything if the value was not found.  
+* If the item is in the array multiple times, it's okay if the program prints the message more than once.
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -12,7 +22,7 @@ public class  FindingAValue{
 
     System.out.print("Array 1: ");
     for(int i = 0; i < arr.length; i++){
-      arr[i] = 1 + rand.nextInt(100);
+      arr[i] = 1 + rand.nextInt(50);
       System.out.print(arr[i] + " ");
     }
     System.out.println(" ");
