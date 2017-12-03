8ba0f04d69975e8046d620e24a77b3e1507f78e6
Quanny Hampton <younghampton@gmail.com>
2017-12-03 15:54:48 -0500

    Update ADumbCalculator.java

--- Sec3-KeyboardInput/ADumbCalculator.java
+++ Sec3-KeyboardInput/ADumbCalculator.java
@@ -4,6 +4,7 @@
 * Make a simple numeric calculator. It should prompt the user for three numbers. Then add the numbers together and
 * divide by 2. Display the result. Your program must support numbers with decimals and not just integers.
 */
+
 import java.util.Scanner;
 public class ADumbCalculator {
 	public static void main(String [] args){
