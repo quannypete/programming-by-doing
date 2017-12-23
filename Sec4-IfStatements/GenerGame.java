eb68f0be7aac082283b1bf3a35a45e763a295554
Quanny Hampton <younghampton@gmail.com>
2017-12-23 03:13:35 -0500

    Update GenerGame.java

--- Sec4-IfStatements/GenerGame.java
+++ Sec4-IfStatements/GenerGame.java
@@ -1,3 +1,16 @@
+/**
+* Gender Game
+* https://programmingbydoing.com/a/gender-game.html
+* Make a program which displays an appropriate name for a person, using a combination of nested ifs and compound conditions.
+* Ask the user for a gender, first name, last name and age.
+* If the person is female and 20 or over, ask if she is married.
+* If so, display "Mrs." in front of her name. If not, display "Ms." in front of her name. 
+* If the female is under 20, display her first and last name.
+* If the person is male and 20 or over, display "Mr." in front of his name.
+* Otherwise, display his first and last name.
+* Note that asking a person if they are married should only be done if they are female and 20 or older, which means you will have a single if and else nested inside one of your if statements.
+*/
+
 import java.util.Scanner;
 public class GenerGame {
 	public static void main(String[] args){
