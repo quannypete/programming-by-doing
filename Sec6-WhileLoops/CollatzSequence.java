2cdfa27c7ee60805787f194b0d3b38ea63ef5ee2
Quanny Hampton <younghampton@gmail.com>
2017-12-23 03:04:23 -0500

    Update CollatzSequence.java

--- Sec6-WhileLoops/CollatzSequence.java
+++ Sec6-WhileLoops/CollatzSequence.java
@@ -1,5 +1,17 @@
-import java.util.Scanner;
+/*
+* Collatz Sequence
+* https://programmingbydoing.com/a/collatz-sequence.html
+* Take any natural number n.
+* If n is even, divide it by 2 to get n / 2.
+* If n is odd, multiply it by 3 and add 1 to get 3n + 1.
+* Repeat the process indefinitely.
+* In 1937, Lothar Collatz proposed that no matter what number you begin with, the sequence eventually reaches 1.
+* This is widely believed to be true, but has never been formally proved.
+* Write a program that inputs a number from the user, and then displays the Collatz Sequence starting from that number. 
+* Stop when you reach 1.
+*/
 
+import java.util.Scanner;
 public class CollatzSequence{
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
