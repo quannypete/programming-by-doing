44c426c9078aba2980a6f4f92564ed5320f6cee2
Quanny Hampton <younghampton@gmail.com>
2017-12-23 03:35:16 -0500

    Update LetterToYourself.py

--- Sec1-BasicsAndPrinting/LetterToYourself.py
+++ Sec1-BasicsAndPrinting/LetterToYourself.py
@@ -1,7 +1,7 @@
 # A Letter to Yourself
 # http://programmingbydoing.com/a/letter-to-yourself.html
-# Write a program that displays your name and address on the screen as if it were a letter. Your output should look
-# something like that below.
+# Write a program that displays your name and address on the screen as if it were a letter. 
+# Your output should look something like that below.
 
 print"+---------------------------------------------------------+"
 print"|                                                    #### |"
