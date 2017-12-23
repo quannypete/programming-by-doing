e9b5a209aad17b18ebee389c9f1a921b50473c26
Quanny Hampton <younghampton@gmail.com>
2017-12-23 03:19:18 -0500

    Update SpaceBoxing.java

--- Sec4-IfStatements/SpaceBoxing.java
+++ Sec4-IfStatements/SpaceBoxing.java
@@ -1,27 +1,24 @@
+/**
+* Space Boxing
+* http://programmingbydoing.com/a/space-boxing.html
+* Julio Cesar Chavez Mark VII is an interplanetary space boxer, who currently holds the championship belts for various weight categories on many different planets within our solar system.
+* However, it is often difficult for him to recall what his "target weight" needs to be on earth in order to make the weight class on other planets.
+* Write a program to help him keep track of this.
+* It should ask him what his earth weight is, and to enter a number for the planet he wants to fight on. 
+* It should then compute his weight on the destination planet based on the table below:
+* #	Planet	Relative gravity
+* 1	Venus	0.78
+* 2	Mars	0.39
+* 3	Jupiter	2.65
+* 4	Saturn	1.17
+* 5	Uranus	1.05
+* 6	Neptune	1.23
+* So, for example, if Julio weighs 128 lbs. on earth, then he would weigh just under 50 lbs. on Mars, since Mars' gravity is 0.39 times earth's gravity. (128 * 0.39 is 49.92)
+*/
+
 import java.util.Scanner;
 public class SpaceBoxing {
 	public static void main(String [] args){
-		
-		/**
-		 * Space Boxing
-		 * http://programmingbydoing.com/a/space-boxing.html
-		 * Julio Cesar Chavez Mark VII is an interplanetary space boxer, who currently holds the championship belts for various
-		 * weight categories on many different planets within our solar system. However, it is often difficult for him to recall
-		 * what his "target weight" needs to be on earth in order to make the weight class on other planets. Write a program to
-		 * help him keep track of this.
-		 * It should ask him what his earth weight is, and to enter a number for the planet he wants to fight on. It should then
-		 * compute his weight on the destination planet based on the table below:
-		 * #	Planet	Relative gravity
-		 * 1	Venus	0.78
-		 * 2	Mars	0.39
-		 * 3	Jupiter	2.65
-		 * 4	Saturn	1.17
-		 * 5	Uranus	1.05
-		 * 6	Neptune	1.23
-		 * So, for example, if Julio weighs 128 lbs. on earth, then he would weigh just under 50 lbs. on Mars, since Mars'
-		 * gravity is 0.39 times earth's gravity. (128 * 0.39 is 49.92)
-		 */
-		
 		int earthweight, planet;
 		double planetweight =0.00;
 		Scanner keyboard = new Scanner(System.in);
@@ -36,6 +33,7 @@ public class SpaceBoxing {
 		System.out.print("Which planet are you visiting? ");
 		planet = keyboard.nextInt();
 		System.out.println("");
+		
 		if(planet == 1){
 			planetweight = earthweight * 0.78;
 		}
