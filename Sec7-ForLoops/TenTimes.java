938c60a5531ae3ec3dabd4f240bf45bbf52ed9dd
Quanny Hampton <younghampton@gmail.com>
2017-12-26 00:54:05 -0500

    Create TenTimes.java

---
+++ Sec7-ForLoops/TenTimes.java
@@ -0,0 +1,15 @@
+/*
+* Ten Times
+* https://programmingbydoing.com/a/ten-times.html
+* Write a program that prints the important phrase "Mr. Mitchell is cool." on the screen ten times.
+* Use a for loop to do it.
+*/
+
+public class TenTimes{
+  public static void main(String[] args) {
+
+    for(int i=1; i <= 10; i++){
+      System.out.println( i + ". Mr. Mitchell is cool.");
+    }
+  }
+}
