395062ebeae25453530e3484934bfeca78b4cb43
Quanny Hampton <younghampton@gmail.com>
2017-12-27 22:06:16 -0500

    Update NoticingEvenNumbers.java

--- Sec7-ForLoops/NoticingEvenNumbers.java
+++ Sec7-ForLoops/NoticingEvenNumbers.java
@@ -1,3 +1,11 @@
+/*
+* Noticing Even Numbers
+* https://programmingbydoing.com/a/noticing-even-numbers.html
+* Write a program that uses a for loop to display all the numbers from 1 to 20, marking those which are even (divisible by two). 
+* It should use modulus by 2: if the remainder is zero, it's divisible by 2.
+* This means you'll need an if statement inside the loop.
+*/
+
 public class NoticingEvenNumbers{
   public static void main(String[] args) {
 
