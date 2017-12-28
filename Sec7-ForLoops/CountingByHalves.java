8fe777f918429f44901d468fb74311d57d5ecb8d
Quanny Hampton <younghampton@gmail.com>
2017-12-27 21:32:12 -0500

    Add files via upload

---
+++ Sec7-ForLoops/CountingByHalves.java
@@ -0,0 +1,12 @@
+public class CountingByHalves{
+public static void main(String[] args) {
+  double increaseBy = 0.5;
+
+  System.out.println("x");
+  System.out.println("------");
+
+  for(double i = -10; i <= 10; i += increaseBy){
+    System.out.println(i);
+  }
+}
+}
