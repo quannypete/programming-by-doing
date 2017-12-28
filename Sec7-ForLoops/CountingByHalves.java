d9789749174d0407dd1e16d3fb3ff371f842a786
Quanny Hampton <younghampton@gmail.com>
2017-12-27 21:49:56 -0500

    Update CountingByHalves.java

--- Sec7-ForLoops/CountingByHalves.java
+++ Sec7-ForLoops/CountingByHalves.java
@@ -1,3 +1,9 @@
+/*
+* Counting By Halves
+* Write a program that uses a for loop. 
+* ith the loop, make the variable x go from -10 to 10, counting by 0.5. (This means that x can't be an int.)
+*/
+
 public class CountingByHalves{
 public static void main(String[] args) {
   double increaseBy = 0.5;
