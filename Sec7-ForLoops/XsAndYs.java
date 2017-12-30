e2f9fbdaec3e3b68af4468bfdda521c1f0fa799f
Quanny Hampton <younghampton@gmail.com>
2017-12-29 21:31:24 -0500

    Update XsAndYs.java

--- Sec7-ForLoops/XsAndYs.java
+++ Sec7-ForLoops/XsAndYs.java
@@ -1,3 +1,12 @@
+/*
+* Xs and Ys
+* https://programmingbydoing.com/a/xs-and-ys.html
+* Write another program that uses a for loop. 
+* With the loop, make the variable x go from -10 to 10, counting by 0.5. (This means that x can't be an int.)
+* Inside the body of the loop, make another variable y become the current value of x squared.
+* Then display the current values of both x and y.
+*/
+
 public class XsAndYs{
 public static void main(String[] args) {
   double increaseBy = 0.5;
