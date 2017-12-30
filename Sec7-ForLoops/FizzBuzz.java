9f50ec9e747715d0bace0661ceec599855366156
Quanny Hampton <younghampton@gmail.com>
2017-12-29 21:44:10 -0500

    Update FizzBuzz.java

--- Sec7-ForLoops/FizzBuzz.java
+++ Sec7-ForLoops/FizzBuzz.java
@@ -1,3 +1,11 @@
+/*
+* Fizz Buzz
+* https://programmingbydoing.com/a/fizzbuzz.html
+* Write a program that prints the numbers from 1 to 100.
+* But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+* For numbers which are multiples of both three and five print "FizzBuzz".
+*/
+
 public class FizzBuzz{
   public static void main(String[] args) {
 
