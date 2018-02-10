4418753d087acd49d73e7a7f17c8c923b43d8522
Quanny Hampton <younghampton@gmail.com>
2018-02-10 12:28:05 -0500

    Add files via upload

---
+++ Sec9-Functions/MonthOffset.java
@@ -0,0 +1,30 @@
+public class MonthOffset
+{
+	public static int month_offset( int month )
+	{
+		int result;
+		// Your code goes in here.
+
+		return result;
+	}
+
+
+	public static void main( String[] args )
+	{
+		System.out.println( "Offset for month 1: " + month_offset(1) );
+		System.out.println( "Offset for month 2: " + month_offset(2) );
+		System.out.println( "Offset for month 3: " + month_offset(3) );
+		System.out.println( "Offset for month 4: " + month_offset(4) );
+		System.out.println( "Offset for month 5: " + month_offset(5) );
+		System.out.println( "Offset for month 6: " + month_offset(6) );
+		System.out.println( "Offset for month 7: " + month_offset(7) );
+		System.out.println( "Offset for month 8: " + month_offset(8) );
+		System.out.println( "Offset for month 9: " + month_offset(9) );
+		System.out.println( "Offset for month 10: " + month_offset(10) );
+		System.out.println( "Offset for month 11: " + month_offset(11) );
+		System.out.println( "Offset for month 12: " + month_offset(12) );
+		System.out.println( "Offset for month 43: " + month_offset(43) );
+	}
+
+
+}
