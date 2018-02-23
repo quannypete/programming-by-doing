3b4656754831c1636ba47c69ba7d5ac1d0524b6a
Quanny Hampton <younghampton@gmail.com>
2018-02-23 16:50:09 -0500

    Update BasicArrayLists1.java

--- Sec16-ArrayList/BasicArrayLists1.java
+++ Sec16-ArrayList/BasicArrayLists1.java
@@ -8,3 +8,32 @@
 * Also, in the condition of your loop, you should not count up to a literal number. 
 * Instead you should use the size() of your ArrayList.
 */
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		int slot;
+
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+		al.add(-113);
+
+		for (int i = 0; i < al.size(); i++) {
+			slot = i;
+			System.out.println("Slot " + slot + " contains a " + al.get(i));
+
+		}
+
+	}
+}
