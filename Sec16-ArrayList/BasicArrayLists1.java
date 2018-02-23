e077ffe88f1a879f1a93c493dc50bb6a31e4076f
Quanny Hampton <younghampton@gmail.com>
2018-02-23 16:49:38 -0500

    Create BasicArrayLists1.java

---
+++ Sec16-ArrayList/BasicArrayLists1.java
@@ -0,0 +1,10 @@
+/*
+* Basic ArrayLists 1
+* https://programmingbydoing.com/a/basic-arraylists-1.html
+* Create an ArrayList that can hold Integers.
+* Add ten copies of the number -113 to the ArrayList. 
+* Then display the contents of the ArrayList on the screen..
+* This time, you must use a loop, both to fill up the ArrayList and to display it.
+* Also, in the condition of your loop, you should not count up to a literal number. 
+* Instead you should use the size() of your ArrayList.
+*/
