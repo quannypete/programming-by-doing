47c859d4668e773394e94f21aa264d404a552a62
Quanny Hampton <younghampton@gmail.com>
2018-02-24 21:19:24 -0500

    Update WhereIsIt.java

--- Sec13-Arrays/WhereIsIt.java
+++ Sec13-Arrays/WhereIsIt.java
@@ -1,3 +1,13 @@
+/*
+* Where Is It?
+* hhttps://programmingbydoing.com/a/where-is-it.html
+* Create an array that can hold ten integers, and fill each slot with a different random value from 1-50.
+* Display those values on the screen, and then prompt the user for an integer.
+* Search through the array, and if the item is present, give the slot number where it is located.
+* If the value is not in the array, display a single message saying so.
+* Just like the previous assignment, if the value is present more than once, you may display the message as many times as necessary.
+*/
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,7 +24,7 @@ public class WhereIsIt{
 
     System.out.println("Array: ");
     for(int i=0; i < myarr.length; i++){
-      int x = rand.nextInt(100);
+      int x = rand.nextInt(50);
       myarr[i] = x;
       System.out.print(myarr[i] + " ");
     }
