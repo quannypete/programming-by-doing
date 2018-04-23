79e96dc7bd062c3a9345a9a47006c180fe45bb4c
Quanny Hampton <younghampton@gmail.com>
2018-04-23 12:29:14 -0400

    Update FindingAValue.java

--- Sec16-ArrayList/FindingAValue.java
+++ Sec16-ArrayList/FindingAValue.java
@@ -1,3 +1,13 @@
+/*
+* Finding a Value in an ArrayList
+* https://programmingbydoing.com/a/finding-a-value-in-an-arraylist.html
+* Create an ArrayList that can hold Integers, and fill each slot with a different random value from 1-50. 
+* Display those values on the screen, and then prompt the user for an integer.  
+* Search through the ArrayList, and if the item is present, say so.
+*  It is not necessary to display anything if the value was not found. 
+* If the item is in the ArrayList multiple times, it's okay if the program prints the message more than once.
+*/
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
