43de87ba4417bdc947e18ffbd759aedf6b681019
Quanny <younghampton@gmail.com>
2018-09-18 13:36:28 -0400

    First commit

---
+++ Sec11-NestedLoops/IndividualDigits.java
@@ -0,0 +1,11 @@
+class IndividualDigits{
+  public static void main(String[] args) {
+
+    for(int i = 1; i <= 9 ; i++){
+      for(int j = 0 ; j<= 9; j++){
+        System.out.println(i +""+ j + ", " + i + "+" +j + " = " + (i+j) );
+      }
+    }
+  }
+
+}
